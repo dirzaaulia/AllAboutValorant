46bfc4db22aaa70b2322f52a5e94ce9a91901c91
Dirza Aulia <dirza.aulia@ibm.com>
2023-03-06 15:25:02 +0700

    Initial commit

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,14 @@
+object AppConfig {
+    const val applicationId = "com.dirzaaulia.baseprojectcompose"
+    const val compileSdk = 33
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName= "1"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    object KeyStore {
+        const val password = "stravinsky9"
+        const val alias = "keydirza"
+    }
+}
